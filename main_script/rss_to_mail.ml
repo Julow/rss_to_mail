211346e59c8c7b083b175c627248bb27d06f8482
juloo <juloo.dsi@gmail.com>
2018-05-29 19:08:31 +0200

    Move the generation of the mail body

--- main_script/rss_to_mail.ml
+++ main_script/rss_to_mail.ml
@@ -113,90 +113,8 @@ let cut_entries since entries =
 			Int64.Infix.(>) d since
 		| None		-> true) entries
 
-let opt_link title = function
-	| Some link	-> "<a href=\"" ^ Uri.to_string link ^ "\">" ^ title ^ "</a>"
-	| None		-> title
-
-let img url styles =
-	let styles = String.concat ";" styles in
-	"<img style=\"" ^ styles ^ "\" src=\"" ^ Uri.to_string url ^ "\" />"
-
 let update_entry feed_url feed options entry =
 	let open Feed in
-	let content =
-		let categories =
-			let labels = List.map (function
-				| { label = Some l; _ }	-> l
-				| { term = Some t; _ }	-> t
-				| _ -> "") entry.categories in
-			match labels with
-			| []		-> ""
-			| l			-> " (" ^ String.concat ", " l ^ ")"
-		and authors =
-			let author a = opt_link a.author_name a.author_link in
-			match List.map author entry.authors with
-			| []		-> ""
-			| authors	-> " by " ^ String.concat ", " authors
-		and date = Option.map_or "" ((^) "on ") entry.date
-		and summary =
-			match entry.content, entry.summary with
-			| Some cont, _		-> cont
-			| None, Some sum	-> "<p>" ^ sum ^ "</p>"
-			| None, None		-> ""
-		and feed_title =
-			let icon = match feed.feed_icon with
-				| Some url	-> img url [
-					"display: inline !important";
-					"height: 1em !important;";
-					"margin: 0 0 -0.1em 0 !important;" ]
-				| None		-> ""
-			and title = match feed.feed_title with
-				| Some title	-> title
-				| None			-> feed_url
-			in
-			opt_link (icon ^ title) feed.feed_link
-		and entry_title =
-			let thumb = match entry.thumbnail with
-				| Some url	-> img url [
-					"display: block !important;";
-					"max-width: 25em;" ]
-				| None		-> ""
-			in
-			let p t = "<p>" ^ opt_link (t ^ thumb) entry.link ^ "</p>" in
-			match entry.title, entry.link with
-			| Some t, _		-> p t
-			| None, Some l	-> p (Uri.to_string l)
-			| None, None	-> ""
-		and label =
-			match options.Feed_options.label with
-			| Some l	-> " with label " ^ l
-			| None		-> ""
-		and attachments =
-			let attachment t =
-				let info =
-					match Option.(to_list (map Utils.size t.attach_size))
-						@ Option.to_list t.attach_type with
-					| []		-> ""
-					| i			-> " (" ^ String.concat ", " i ^ ")"
-				in
-				let title =
-					match String.Split.right ~by:"/" (Uri.path t.attach_url) with
-					| Some (_, title) when String.contains title '.' -> title
-					| _			-> Uri.to_string t.attach_url
-				in
-				let link = opt_link title (Some t.attach_url) in
-				"<p>Attachment: " ^ link ^ info ^ "</p>"
-			in
-			String.concat "" (List.map attachment entry.attachments)
-		in
-		String.concat "" [
-			"<p>Via "; feed_title; categories; "<br/>";
-			date; authors; label; "</p>";
-			entry_title;
-			attachments;
-			summary
-		]
-	in
 	let id = match entry.id, entry.link, entry.title with
 		| Some id, _, _				-> Some (feed_url ^ id)
 		| None, Some link, _		-> Some (feed_url ^ Uri.to_string link)
@@ -206,8 +124,8 @@ let update_entry feed_url feed options entry =
 		| Some _ as title, _	-> title
 		| None, Some link		-> Some (Uri.to_string link)
 		| None, None			-> Some feed_url
-	in
-	{ entry with id; title; summary = None; content = Some content }
+	and content = Some (Mail_body.generate feed options entry) in
+	{ entry with id; title; summary = None; content }
 
 let parse_feed contents =
 	Feed_parser.parse (Xmlm.make_input (`String (0, Js.to_string contents)))
---
+++ rss_to_mail/mail_body.ml
@@ -0,0 +1,83 @@
+open Feed
+
+let opt_link title = function
+	| Some link	-> "<a href=\"" ^ Uri.to_string link ^ "\">" ^ title ^ "</a>"
+	| None		-> title
+
+let img url styles =
+	let styles = String.concat ";" styles in
+	"<img style=\"" ^ styles ^ "\" src=\"" ^ Uri.to_string url ^ "\" />"
+
+let generate feed options entry =
+	let categories =
+		let labels = List.map (function
+			| { label = Some l; _ }	-> l
+			| { term = Some t; _ }	-> t
+			| _ -> "") entry.categories in
+		match labels with
+		| []		-> ""
+		| l			-> " (" ^ String.concat ", " l ^ ")"
+	and authors =
+		let author a = opt_link a.author_name a.author_link in
+		match List.map author entry.authors with
+		| []		-> ""
+		| authors	-> " by " ^ String.concat ", " authors
+	and date = Option.map_or "" ((^) "on ") entry.date
+	and summary =
+		match entry.content, entry.summary with
+		| Some cont, _		-> cont
+		| None, Some sum	-> "<p>" ^ sum ^ "</p>"
+		| None, None		-> ""
+	and feed_title =
+		let icon = match feed.feed_icon with
+			| Some url	-> img url [
+				"display: inline !important";
+				"height: 1em !important;";
+				"margin: 0 0 -0.1em 0 !important;" ]
+			| None		-> ""
+		and title = match feed.feed_title with
+			| Some title	-> title
+			| None			-> ""
+		in
+		opt_link (icon ^ title) feed.feed_link
+	and entry_title =
+		let thumb = match entry.thumbnail with
+			| Some url	-> img url [
+				"display: block !important;";
+				"max-width: 25em;" ]
+			| None		-> ""
+		in
+		let p t = "<p>" ^ opt_link (t ^ thumb) entry.link ^ "</p>" in
+		match entry.title, entry.link with
+		| Some t, _		-> p t
+		| None, Some l	-> p (Uri.to_string l)
+		| None, None	-> ""
+	and label =
+		match options.Feed_options.label with
+		| Some l	-> " with label " ^ l
+		| None		-> ""
+	and attachments =
+		let attachment t =
+			let info =
+				match Option.(to_list (map Utils.size t.attach_size))
+					@ Option.to_list t.attach_type with
+				| []		-> ""
+				| i			-> " (" ^ String.concat ", " i ^ ")"
+			in
+			let title =
+				match String.Split.right ~by:"/" (Uri.path t.attach_url) with
+				| Some (_, title) when String.contains title '.' -> title
+				| _			-> Uri.to_string t.attach_url
+			in
+			let link = opt_link title (Some t.attach_url) in
+			"<p>Attachment: " ^ link ^ info ^ "</p>"
+		in
+		String.concat "" (List.map attachment entry.attachments)
+	in
+	String.concat "" [
+		"<p>Via "; feed_title; categories; "<br/>";
+		date; authors; label; "</p>";
+		entry_title;
+		attachments;
+		summary
+	]
