64c5147d2f7cc92f2bd4993b92147d1a95b0de65
Nathan Rebours <nathan.p.rebours@gmail.com>
2019-05-07 16:11:19 +0200

    Declare a few unikernel dependencies

--- main_mirage/config.ml
+++ main_mirage/config.ml
@@ -1,7 +1,14 @@
 open Mirage
 
 let main =
-  foreign "Unikernel.Main" job
+  foreign
+    "Unikernel.Main"
+    ~packages:[ package "cohttp-mirage" ]
+    (time @-> resolver @-> conduit @-> kv_rw @-> job)
 
 let () =
-  register "rss_to_mail" [main]
+  let stack = generic_stackv4 default_network in
+  let res_dns = resolver_dns stack in
+  let conduit = conduit_direct stack in
+  let kv_rw = direct_kv_rw "persistent_storage" in
+  register "rss_to_mail" [ main $ default_time $ res_dns $ conduit $ kv_rw ]
--- main_mirage/unikernel.ml
+++ main_mirage/unikernel.ml
@@ -1,3 +1,11 @@
-module Main = struct
-  let start = Lwt.return_unit
+open Mirage_types_lwt
+
+module Main
+    (Time : TIME)
+    (Resolver: Resolver_lwt.S)
+    (Conduit: Conduit_mirage.S)
+    (Kv_rw: Mirage_kv_lwt.RW)
+= struct
+  let start _time _res_dns _conduit _kv_rw =
+    Lwt.return_unit
 end
