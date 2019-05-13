197f9ff51d60d9775a8e1eb406c8f671c4bb6364
juloo <juloo.dsi@gmail.com>
2019-05-13 23:44:35 +0200

    Instantiate Rss_to_mail, do fetch

--- main_mirage/config.ml
+++ main_mirage/config.ml
@@ -1,14 +1,18 @@
 open Mirage
 
+let packages = [
+  package "cohttp-mirage";
+  package "rss_to_mail";
+  package ~sublibs:["mirage"] "tls";
+]
+
 let main =
-  foreign
-    "Unikernel.Main"
-    ~packages:[ package "cohttp-mirage" ]
+  foreign "Unikernel.Main" ~packages
     (time @-> resolver @-> conduit @-> kv_rw @-> job)
 
 let () =
   let stack = generic_stackv4 default_network in
   let res_dns = resolver_dns stack in
-  let conduit = conduit_direct stack in
+  let conduit = conduit_direct ~tls:true stack in
   let kv_rw = direct_kv_rw "persistent_storage" in
   register "rss_to_mail" [ main $ default_time $ res_dns $ conduit $ kv_rw ]
