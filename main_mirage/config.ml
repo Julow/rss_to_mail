b82ec3afe55c6372bd083381dc63ed224bb8c79e
juloo <juloo.dsi@gmail.com>
2019-05-15 00:52:19 +0200

    enable tls

--- main_mirage/config.ml
+++ main_mirage/config.ml
@@ -13,6 +13,6 @@ let main =
 let () =
   let stack = generic_stackv4 default_network in
   let res_dns = resolver_dns stack in
-  let conduit = conduit_direct stack in
+  let conduit = conduit_direct ~tls:true stack in
   let kv_rw = direct_kv_rw "persistent_storage" in
   register "rss_to_mail" [ main $ default_time $ res_dns $ conduit $ kv_rw ]
